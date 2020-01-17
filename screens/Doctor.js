9689944f4ba75fc2bcbb6b9a39490a18b579d68c
Phakhin Deesee <phakhind48@gmail.com>
2020-01-17 20:23:20 +0700

    add QRCode reader

--- screens/Doctor.js
+++ screens/Doctor.js
@@ -1,14 +1,50 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import {Text, View, TouchableOpacity, Linking, StyleSheet} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
 class Doctor extends React.Component {
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err),
+    );
+  };
+
   render() {
     return (
-      <View>
-        <Text style={{fontSize: 50, color: '#4CD5FF', textAlign: 'center'}}>
-          I am Doctor.
-        </Text>
-      </View>
+        <QRCodeScanner
+          onRead={this.onSuccess}
+          topContent={
+            <Text style={styles.centerText}>
+              Scan Doctor's QRCode.
+            </Text>
+          }
+          bottomContent={
+            <TouchableOpacity style={styles.buttonTouchable}>
+              <Text style={styles.buttonText}>OK. Got it!</Text>
+            </TouchableOpacity>
+          }
+        />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
 export default Doctor;
